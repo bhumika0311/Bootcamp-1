04cb17246a8b18d934b66d2ba5eeaf790dac480f
Bhumika Saxena <bhumika056btcse17@igdtuw.ac.in>
2019-06-10 01:22:16 +0530

    Made further changes

--- Weekend_01_Homework/DateToNum.py
+++ Weekend_01_Homework/DateToNum.py
@@ -8,7 +8,7 @@ monthNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 def isLeapYear(year):
   year = (int)(year)
-  return (year % 400 == 0) or (year % 4 == 0 and year % 100 !=0)
+  return (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0)
 
 #########################################################################
 
@@ -18,28 +18,23 @@ def DateToNumber(date):
   currentMonth = dict(zip(monthNames, monthNumber))[date[1]]
 
   for (x, y) in zip(monthNumber, numberOfDaysInEveryMonth):
-     if x < currentMonth:
-       numberOfDays += y
+    if x < currentMonth:
+      numberOfDays += y
 
   numberOfDays += (int)(date[0])
 
   if isLeapYear(date[2]) and currentMonth > 2:
     numberOfDays += 1
 
-  numberOfDays =  str(numberOfDays)
+  numberOfDays = str(numberOfDays)
+  endNumber = date[2] + numberOfDays
 
-  currentYear = date[2]
+  while (len(endNumber) < 7):
+    endNumber = endNumber[:4] + str(0) + endNumber[4:]
 
-  if len(numberOfDays) == 2:
-      print(currentYear + '0' + numberOfDays)
-  elif len(numberOfDays) == 1:
-    print(currentYear + '00' + numberOfDays)
-  else:
-    print(currentYear + numberOfDays)
-    
+  print(endNumber)
 
 #############################################################################
 
-
-DateToNumber("7 Jul,2020")
+DateToNumber("7 Mar,2020")
 
