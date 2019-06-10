4e34814c214f99c41f089f641ff8a53edb47f40b
Bhumika Saxena <bhumika056btcse17@igdtuw.ac.in>
2019-06-10 12:32:25 +0530

    Removal of function

--- Week02Day01/Anagram.py
+++ Week02Day01/Anagram.py
@@ -3,20 +3,21 @@ occurrences = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 mapping1 = dict(zip(alphabets, occurrences))
 mapping2 = dict(zip(alphabets, occurrences))
 
-def isSameLength(string1, string2):
-  return len(string1) == len(string2)
-
 def isAnagram (string1, string2):
-    n = 0
-    while n < len(string1):
-      mapping1[string1.upper()[n]] += 1
-      n += 1
 
-    m = 0
-    while m < len(string2):
-      mapping2[string2.upper()[m]] += 1
-      m += 1
+  string1 = string1.replace(' ', '')
+  string2 = string2.replace(' ', '')
 
-    return mapping1 == mapping2
+  n = 0
+  while n < len(string1):
+    mapping1[string1.upper()[n]] += 1
+    n += 1
 
-print(isAnagram('Abc', 'Bcaa'))
+  m = 0
+  while m < len(string2):
+    mapping2[string2.upper()[m]] += 1
+    m += 1
+
+  return mapping1 == mapping2
+
+print(isAnagram('Abc', 'Bc a'))
