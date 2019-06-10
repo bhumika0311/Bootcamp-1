ce07941e4ff4cc498e7f1cf5612a83c43401404d
Bhumika Saxena <bhumika056btcse17@igdtuw.ac.in>
2019-06-10 11:40:18 +0530

    Removed a conditional

--- Week02Day01/Anagram.py
+++ Week02Day01/Anagram.py
@@ -7,19 +7,16 @@ def isSameLength(string1, string2):
   return len(string1) == len(string2)
 
 def isAnagram (string1, string2):
-    if isSameLength(string1, string2):
-      n = 0
-      while n < len(string1):
-        mapping1[string1.upper()[n]] += 1
-        n += 1
+    n = 0
+    while n < len(string1):
+      mapping1[string1.upper()[n]] += 1
+      n += 1
 
-      m = 0
-      while m < len(string2):
-        mapping2[string2.upper()[m]] += 1
-        m += 1
+    m = 0
+    while m < len(string2):
+      mapping2[string2.upper()[m]] += 1
+      m += 1
 
-      return mapping1 == mapping2
-    
-    return False
+    return mapping1 == mapping2
 
-print(isAnagram('Abc', 'Bcd'))
+print(isAnagram('Abc', 'Bcaa'))
