13380aeac6a59deeef606d1a11fde824215e1a55
Bhumika Saxena <bhumika056btcse17@igdtuw.ac.in>
2019-06-10 17:06:30 +0530

    Added code

---
+++ Assessment02/ambiguous.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include <string>
+using namespace std;
+
+	int convertToInteger(string str1) {
+		return std :: str1();
+	}
+
+	bool isAmbiguous(string str1) {
+		return convertToInteger(str1[0, 2]) <= 12 && convertToInteger(str1[3, 5]) <= 12;
+
+	}
+
+	int main(){
+		System.out.println(isAmbiguous("12/11/2018"));
+		System.out.println(isAmbiguous("11/02/2018"));
+		System.out.println(isAmbiguous("30/11/2018"));
+		System.out.println(isAmbiguous("11/31/2018"));
+	}
+
+}
+
