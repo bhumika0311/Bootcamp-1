104aa8847bee5792ffb1f471cc54d18a5c05fb2e
Bhumika Saxena <bhumika056btcse17@igdtuw.ac.in>
2019-06-14 16:52:13 +0530

    changes

--- Assessment04/Terminus.py
+++ Assessment04/Terminus.py
@@ -7,22 +7,27 @@ print(pathsLeftRight)
 print(pathsUpDown)
 def Terminus (latticePoint, listOfStrings):
     i = 0
+    j = 0
     x = latticePoint[0]
     y = latticePoint[1]
+    print(pathsUpDown.get(listOfStrings[0][-1]))
     while i < len(listOfStrings):
-        if [listOfStrings[i][-1]] in DirectionsLeftRight:
-            x += pathsLeftRight.get([listOfStrings[i][-1]])
-            
-        elif [listOfStrings[i][-1]] in DirectionsUpDown:
-            y += pathsUpDown.get([listOfStrings[i][-1]])
-            
-        elif [listOfStrings[i][-2]] in DirectionsLeftRight:
-            x += pathsLeftRight.get([listOfStrings[i][-2]])
-            
-        elif [listOfStrings[i][-2]] in DirectionsUpDown:
-            y += pathsUpDown.get([listOfStrings[i][-2]])
+        while j < (int)(listOfStrings[i][0: -1]):
+            if [listOfStrings[i][-1]] in DirectionsLeftRight:
+                x += pathsLeftRight.get(listOfStrings[i][-1])
+                
+            elif [listOfStrings[i][-1]] in DirectionsUpDown:
+                y += pathsUpDown.get(listOfStrings[i][-1])
+                
+            elif [listOfStrings[i][-2]] in DirectionsLeftRight:
+                x += pathsLeftRight.get(listOfStrings[i][-2])
+                
+            elif [listOfStrings[i][-2]] in DirectionsUpDown:
+                y += pathsUpDown.get(listOfStrings[i][-2])
+
+            j += 1
             
         i += 1
     return (x, y)
     
-print(Terminus((1, 1), ["1N", "3NW"]))
+print(Terminus((1, 1), ["1N", "3W"]))
