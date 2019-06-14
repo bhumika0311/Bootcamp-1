246a3202bdfb9f37524cb713273852951689bbbc
Bhumika Saxena <bhumika056btcse17@igdtuw.ac.in>
2019-06-14 16:45:54 +0530

    changes

--- Assessment04/Terminus.py
+++ Assessment04/Terminus.py
@@ -3,20 +3,24 @@ Movements = [1, -1]
 DirectionsLeftRight = ["E", "W"]
 pathsLeftRight = dict(zip(DirectionsLeftRight, Movements))
 pathsUpDown = dict(zip(DirectionsUpDown, Movements))
-
+print(pathsLeftRight)
+print(pathsUpDown)
 def Terminus (latticePoint, listOfStrings):
     i = 0
     x = latticePoint[0]
     y = latticePoint[1]
     while i < len(listOfStrings):
         if [listOfStrings[i][-1]] in DirectionsLeftRight:
-            x += pathsLeftRight[listOfStrings[i][-1]]
+            x += pathsLeftRight.get([listOfStrings[i][-1]])
+            
         elif [listOfStrings[i][-1]] in DirectionsUpDown:
-            y += pathsUpDown[listOfStrings[i][-1]]
-        elif [listOfStrings[i][-1]] in DirectionsLeftRight:
-            x += pathsLeftRight[listOfStrings[i][-1]]
+            y += pathsUpDown.get([listOfStrings[i][-1]])
+            
+        elif [listOfStrings[i][-2]] in DirectionsLeftRight:
+            x += pathsLeftRight.get([listOfStrings[i][-2]])
+            
         elif [listOfStrings[i][-2]] in DirectionsUpDown:
-            y += pathsUpDown[listOfStrings[i][-1]]
+            y += pathsUpDown.get([listOfStrings[i][-2]])
             
         i += 1
     return (x, y)
